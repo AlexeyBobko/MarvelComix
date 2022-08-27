7d3dc15a842f3b567647593dbabff7d9fb064ec2
Alex <lexabobko@gmail.com>
2022-08-27 20:10:03 +0300

    Add spinner and error message

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -28,7 +28,7 @@ class MarvelService {
     _transformCharacter = (char) => {
         return {
             name: char.name,
-            description: char.description,
+            description: char.description ? `${char.description.slice(0, 200)}...` : 'There is no description for this character',
             thumbnail: `${char.thumbnail.path}.${char.thumbnail.extension}`,
             homepage: char.urls[0].url,
             wiki: char.urls[1].url
@@ -37,3 +37,7 @@ class MarvelService {
 }
 
 export default MarvelService;
+//здесь нужно указать num т.к. нужен аргумент
+const numbers = [1, 4, 9];
+const doubles = numbers.map((num) => num * 2);
+// теперь doubles равен [2, 8, 18], а numbers всё ещё равен [1, 4, 9]
